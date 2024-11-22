77bb7e1e0fc412cd0e8806b405048a43b424b769
Groinkb <benjamin@mossu.ch>
2024-11-22 13:17:06 +0100

    remove +

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,6 +1,7 @@
 int fact(int f) {
-	if (f <= 1) {
-		return 1;}
-	int result = f * fact(f+1);
-	return result;
+  if (f <= 1) {
+    return 1;
+  }
+  int result = f * fact(f - 1);
+  return result;
 }
