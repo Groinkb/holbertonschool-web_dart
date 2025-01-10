c9e5272375e5adaa25581852b2657ced6ecc416c
Groinkb <benjamin@mossu.ch>
2025-01-10 15:04:24 +0100

    add : commentary

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,6 +1,6 @@
 import '0-util.dart';
 
 Future<void> usersCount() async {
-  int count = await fetchUsersCount();
+  int count = await fetchUsersCount(); //une fois que future est complétée
   print(count);
 }
