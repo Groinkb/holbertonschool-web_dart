21b2d76b3fead0bb020a6e5de0d098b091afeb31
Groinkb <benjamin@mossu.ch>
2025-04-11 12:42:59 +0200

    correct ?

--- 0x00-hello-dart/0-hello_holberton.dart
+++ 0x00-hello-dart/0-hello_holberton.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
 void main() {
-  // Using a carriage return and newline to match Windows line ending format
-  stdout.write('Hello Holberton!\r\n');
+  print("Hello Holberton!\n");
 }
