8ec520f5872ec0323fdecc824273901ec7fd6ac1
Groinkb <benjamin@mossu.ch>
2025-04-11 15:45:00 +0200

    ouiouiuiuiuiu

--- 0x03-asynchronous/0-util.dart
+++
@@ -1,8 +0,0 @@
-import '1-util.dart';
-import 'dart:convert';
-
-Future<String> getUserId() async {
-  final userData = await fetchUserData();
-  final userMap = json.decode(userData);
-  return userMap['id'];
-}
